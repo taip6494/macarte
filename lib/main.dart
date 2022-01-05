fc13a2522d4554b25adf4e45a1161f5ed18f16b5
taip6494 <palisseauthomas@gmail.com>
2022-01-05 11:41:50 +0400

    Versioin2

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,58 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(
-    MaterialApp(
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
       home: Scaffold(
         backgroundColor: Colors.teal,
-        body: Container(),
+        body: SafeArea(
+          child: Container(
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Container(
+                  color: Colors.redAccent,
+                  height: 730,
+                  width: 100,
+                ),
+                Container(
+                  height: 730,
+                  width: 100,
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Container(
+                        width: 100,
+                        height: 100,
+                        color: Colors.amber,
+                      ),
+                      Container(
+                        width: 100,
+                        height: 100,
+                        color: Colors.green,
+                      ),
+                    ],
+                  ),
+                ),
+                Container(
+                  color: Colors.lightBlueAccent,
+                  height: 730,
+                  width: 100,
+                ),
+
+              ],
+            ),
+          ),
+        ),
       ),
-    ),
-  );
+    );
+  }
 }
+
